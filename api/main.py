6ef4c27b02fefb01b8fe9ab5ad245dd500212c0d
Mojori <vbberchenko@gmail.com>
2025-01-27 19:32:58 +0300

    added exel parser

---
+++ api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+
+from api.routers.exel_parser import exel_parser_router
+
+load_dotenv()
+
+app = FastAPI()
+
+app.include_router(exel_parser_router, prefix="/api/exel_parser", tags=["parsers"])
+
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
