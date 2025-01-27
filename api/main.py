25324f37ce9997efba6039c15e03d8f4504447c4
Mojori <vbberchenko@gmail.com>
2025-01-27 19:50:57 +0300

    commented running a file from main

--- api/main.py
+++ api/main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 
 app.include_router(exel_parser_router, prefix="/api/exel_parser", tags=["parsers"])
 
-import uvicorn
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+# import uvicorn
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
