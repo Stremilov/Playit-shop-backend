190f40d608c7e088f8837685d1e6769da375cd22
Mojori <vbberchenko@gmail.com>
2025-01-27 20:38:23 +0300

    restructuring and template for the users points system

--- api/main.py
+++ api/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
-from api.routers.exel_parser import exel_parser_router
+from api.routers.exel.exel_parser import exel_parser_router
+from api.routers.users.user_points import user_points_router
 
 load_dotenv()
 
+
 app = FastAPI()
 
-app.include_router(exel_parser_router, prefix="/api/exel_parser", tags=["parsers"])
+app.include_router(exel_parser_router, prefix="/api/exel/parser", tags=["exel"])
+app.include_router(user_points_router, prefix="/api/users", tags=["users"])
 
 # import uvicorn
 # if __name__ == "__main__":
