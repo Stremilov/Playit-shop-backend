1ed42b3f51d426c85d93f1009f498a4001f423e1
cl7paBka <maks-kandikov@yandex.ru>
2025-04-05 00:28:45 +0300

    Добавил новый метод, чтобы получить username для верификации jwt-токена

--- src/core/repositories/users.py
+++ src/core/repositories/users.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException
@@ -18,3 +20,21 @@ class UserRepository:
         # Достаём теперь не кортеж, а именно int - число коинов пользователя
         row = result.fetchone()
         return row[0] if row else 0  # Возвращаем первый элемент кортежа (balance) или 0, если пользователь не найден
+
+    @staticmethod
+    async def get_user_by_username(db: AsyncSession, username: str) -> Optional[str]:
+        stmt = text("""
+                    select username
+                    from users
+                    where username = :username
+                    """)
+        try:
+            result = await db.execute(stmt, {"username": username})
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Ошибка при запросе в БД {str(e)}")
+
+        row = result.fetchone()
+
+        return row[0] if row else None
+
+
