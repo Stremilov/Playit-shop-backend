0a9caf47053e572e4be6a18f25e1eb3c2f4e3fa6
cl7paBka <maks-kandikov@yandex.ru>
2025-04-05 00:28:16 +0300

    Логика декодирования и верификация jwt-токена

--- src/core/jwt/tokens.py
+++ src/core/jwt/tokens.py
@@ -1,6 +1,6 @@
 from fastapi_users.jwt import decode_jwt
 from fastapi import HTTPException
-from src.core.utils.config import SECRET_KEY
+from src.core.utils.config import settings
 
 
 def verify_jwt_token(token: str) -> dict:
@@ -8,7 +8,7 @@ def verify_jwt_token(token: str) -> dict:
     Синхронно декодирует и проверяет JWT-токен.
     """
     try:
-        audience = "prod"
-        return decode_jwt(token, SECRET_KEY, audience)
+        audience = "prod"  # Должно совпадать с "aud" в create_jwt_token
+        return decode_jwt(token, settings.token.SECRET_KEY, audience)
     except Exception as e:
         raise HTTPException(status_code=401, detail=f"Невалидный токен: {str(e)}")
--- src/core/utils/auth.py
+++ src/core/utils/auth.py
@@ -1,9 +1,11 @@
 from fastapi import HTTPException, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.jwt.tokens import verify_jwt_token
+from src.core.repositories.users import UserRepository
 
 
-async def verify_user_by_jwt(request: Request):
+async def verify_user_by_jwt(request: Request, session: AsyncSession):
     """
     Возвращает пользователя после аутентификации по username с помощью jwt
     """
@@ -13,5 +15,8 @@ async def verify_user_by_jwt(request: Request):
 
     verified_token = verify_jwt_token(token)
 
-    telegram_id = verified_token.get("telegram_id")
-    return UserRepository.get_user_by_telegram_id(telegram_id=telegram_id)
+    username_from_jwt = verified_token.get("sub")
+    username_from_db = UserRepository.get_user_by_username(db=session, username=username_from_jwt)
+    if username_from_db is None or username_from_jwt != username_from_db:
+        raise HTTPException(status_code=401, detail="По такому имени в JWT-Токене нет пользователя в базе данных")
+
