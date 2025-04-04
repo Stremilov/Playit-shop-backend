f5624e3e05b9aa780ba30ad76a31052d1326d863
Lef <116380867+Stremilov@users.noreply.github.com>
2025-04-05 01:35:24 +0300

    test

--- src/core/utils/auth.py
+++ src/core/utils/auth.py
@@ -14,8 +14,10 @@ async def verify_user_by_jwt(request: Request, session: AsyncSession):
         raise HTTPException(status_code=401, detail="Не авторизован")
 
     verified_token = verify_jwt_token(token)
+    print(verified_token)
 
     username_from_jwt: str = verified_token.get("sub")
+    print(username_from_jwt)
     username_from_db = UserRepository.get_user_by_username(db=session, username=username_from_jwt)
     if username_from_db is None or username_from_jwt != username_from_db:
         raise HTTPException(status_code=401, detail="По такому имени в JWT-Токене нет пользователя в базе данных")
