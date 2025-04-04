0989690b1b0fadb09d365245d8402e356cc5d4f8
Lef <116380867+Stremilov@users.noreply.github.com>
2025-04-05 01:23:13 +0300

    test

--- src/core/jwt/tokens.py
+++ src/core/jwt/tokens.py
@@ -1,7 +1,6 @@
 from fastapi_users.jwt import decode_jwt
 from fastapi import HTTPException
 from src.core.utils.config import settings
-from jose import JWTError
 
 
 def verify_jwt_token(token: str) -> dict:
@@ -10,9 +9,8 @@ def verify_jwt_token(token: str) -> dict:
     """
     try:
         audience = "prod"  # Должно совпадать с "aud" в create_jwt_token
+        print(type(token))
         print(token)
-        return decode_jwt(token, settings.token.SECRET_KEY, audience)
-    except JWTError as e:
-        raise ValueError(f"Ошибка декодирования: {str(e)}")
+        return decode_jwt(str(token), settings.token.SECRET_KEY, audience)
     except Exception as e:
         raise HTTPException(status_code=401, detail=f"Невалидный токен: {str(e)}")
