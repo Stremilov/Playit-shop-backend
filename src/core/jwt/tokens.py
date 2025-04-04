1cf41c0c1d6770441b35e0179c39b9216702b46f
Lef <116380867+Stremilov@users.noreply.github.com>
2025-04-05 01:24:31 +0300

    test

--- src/core/jwt/tokens.py
+++ src/core/jwt/tokens.py
@@ -7,10 +7,10 @@ def verify_jwt_token(token: str) -> dict:
     """
     Синхронно декодирует и проверяет JWT-токен.
     """
-    try:
-        audience = "prod"  # Должно совпадать с "aud" в create_jwt_token
-        print(type(token))
-        print(token)
-        return decode_jwt(str(token), settings.token.SECRET_KEY, audience)
-    except Exception as e:
-        raise HTTPException(status_code=401, detail=f"Невалидный токен: {str(e)}")
+    # try:
+    audience = "prod"  # Должно совпадать с "aud" в create_jwt_token
+    print(type(token))
+    print(token)
+    return decode_jwt(str(token), settings.token.SECRET_KEY, audience)
+    # except Exception as e:
+    #     raise HTTPException(status_code=401, detail=f"Невалидный токен: {str(e)}")
